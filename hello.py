fe1165209d32c9915604e5dd1507a13692d281df
libonge <157608953+libonge@users.noreply.github.com>
2025-05-22 01:13:28 +0200

    Add bootcamp task

---
+++ hello.py
@@ -0,0 +1,12 @@
+# Request user for name
+# Store user input in a variable called name
+# Print name 
+name = input("What is your name? ")
+
+# Request user for age
+# Store user input in a variable called age
+# Print age
+age = input("How old are you? ")
+
+# Print the custom message
+print(f"Hello world, this is {name} and is {age} years old.")
