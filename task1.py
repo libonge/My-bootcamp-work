001b4bfc208415fc50cda40d994d59c84dd2c636
libonge <157608953+libonge@users.noreply.github.com>
2025-05-22 11:51:20 +0200

    Add bootcamp task

---
+++ task1.py
@@ -0,0 +1,13 @@
+#Create a variable called num and set it to 5
+
+num = 5 
+ 
+# increasing star pattern
+for i in range(1, num*(4-2)):
+  if i <= num:
+     print('*' * i )
+     
+# Decreasing stars pattern
+  else:
+      
+    print('*' * (num*(4-2) -i ))
