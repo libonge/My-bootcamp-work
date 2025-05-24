e18c2342d7a6d2a7dd960880b695d39759a0f098
libonge <157608953+libonge@users.noreply.github.com>
2025-05-25 00:55:48 +0200

    Add bootcamp task

---
+++ conversion.py
@@ -0,0 +1,44 @@
+# create a variable called "num1" and store 99.23 into the variable .
+
+num1 = 99.23
+
+# create a variable called "num2" and store 23 into the variable.
+
+num2 = 23
+
+# create a variable called "num3" and store 150 into the variable.
+
+num3 = 150
+
+# create a variable called "string1" and store 100 into into the variable.
+
+string1 = "100"
+
+# Conversion
+
+# Type in a a "import math" to alert python that we are gonna use some of the math module functions.
+
+import math
+
+ # num1 into to an integer
+
+# To covert "num1" into in a integer  use the "int" function.
+
+print(num1,   '             '   ,int(num1))
+
+# num2 into an float
+
+# To convert "num2" into in a integer use the float function.
+
+print(num2,   '             '   ,float(num2))
+
+# num3 into a string 
+#To convert "num3" into a string use the "str" function.
+
+print(num3,   '              '   ,type(str(num3)))
+
+ # num4 into an integer 
+ #To convert "string1" into a string use the int method.
+
+print(string1,  '             '   ,type(int(string1)))
+
