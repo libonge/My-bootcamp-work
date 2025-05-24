94baf724feb1b66621050b966aa491052b1d731a
libonge <157608953+libonge@users.noreply.github.com>
2025-05-25 01:15:00 +0200

    Add bootcamp task

---
+++ numbers.py
@@ -0,0 +1,31 @@
+#  Request the user to enter the first an integer
+
+num1 = int(input("please enter integer num1 : "))
+
+#  Request user to enter the second integer
+
+num2 = int(input("please enter integer num2 : "))
+
+#  Request user to enter the third integer 
+
+num3 = int(input("please enter integer num3 : "))
+
+# Print out the sum of the 3 integer 
+
+sum_of_all_three_integers = num1 + num2 +num3
+print(sum_of_all_three_integers)
+
+# print out the the first number minus the second number
+
+num1_minus__num2 = num1 - num2
+print(num1_minus__num2)
+
+# print out the third number multiplied by the first number
+
+num3_multilpied_num1 = num3 * num1
+print(num3_multilpied_num1)
+
+# print out the sum_of_all_three_integers divided by the third number
+
+sum_of_all_three_integers_divided_by_third_number = sum_of_all_three_integers / num3 
+print(sum_of_all_three_integers_divided_by_third_number)
